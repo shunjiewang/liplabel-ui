6f27e013aae68a678907209a08f1b530c0ef8e24
Shunjie Wang <shunjiewang@hotmail.com>
2019-05-05 10:43:31 -0700

    Eliminate global variables.

--- liplabeler.py
+++ liplabeler.py
@@ -11,19 +11,12 @@ import imgphon
 
 m_tp_list = [5.000, 10.110, 15.001, 20.001, 25.010]
 
-result_dict = {}
-clicks_list = []
-working_directory = ""
-
-
-def interface(windowName, canvas, tp, frame_index, file_name):
+def interface(windowName, canvas, tp, frame_index, file_name, result_dict, clicks_list, working_directory):
     dir_name = "output_imgs"
 
     with contextlib.suppress(FileExistsError):
         os.mkdir(os.path.join(working_directory, dir_name))
 
-    global clicks_list
-    global result_dict
     shouldReload = False
     output_img_name = str(file_name) + "-" + str(frame_index) + "-" + str(tp)
     while True:
@@ -32,7 +25,6 @@ def interface(windowName, canvas, tp, frame_index, file_name):
         if key == 110:  # [N] to next
             if len(clicks_list) != 4:
                 shouldReload = True
-                clicks_list = []
                 break
             cv2.imwrite(os.path.join(working_directory, dir_name,
                                      output_img_name + ".bmp"), canvas)
@@ -54,65 +46,52 @@ def interface(windowName, canvas, tp, frame_index, file_name):
                                  "result_dict.npy"), result_dict)
             sys.exit(0)
     cv2.destroyAllWindows()
-    return shouldReload
+    return (shouldReload, result_dict)
 
 def paint_dot(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(param, (x, y), 3, (60, 20, 220), -1)
-        clicks_list.append((x, y))
+        cv2.circle(param[0], (x, y), 3, (60, 20, 220), -1)
+        param[1].append((x, y))
 
 
-def label_single(frame_index, vid_path):
+def label_single(frame_index, vid_path, total_frames_count, result_dict, clicks_list, working_directory):
     shouldReload = True
     tp = m_tp_list[frame_index - 1]
     file_name = os.path.basename(vid_path)
     imgphon.get_video_frame(vid_path, tp)
+    m_clicks_list = clicks_list
 
     while shouldReload:
         curr_frame = cv2.imread("temp.bmp")
-        windowName = str(file_name) + " (Frame " + str(frame_index) + ": " + str(tp) + \
-            ") | [R] Reload | [N] Save & Next | [Q] Save & Quit"
+        windowName = "(Frame " + str(frame_index) + " of " + str(total_frames_count) + ") " + str(file_name) + " @ " + str(tp) +  \
+            " Sec | [R] Reload | [N] Save & Next | [Q] Save & Quit"
         cv2.namedWindow(windowName)
         cv2.moveWindow(windowName, 320, 180)
-        cv2.setMouseCallback(windowName, paint_dot, curr_frame)
+        cv2.setMouseCallback(windowName, paint_dot, (curr_frame, m_clicks_list))
 
-        shouldReload = interface(windowName, curr_frame,
-                                 tp, frame_index, file_name)
-
+        interface_return = interface(windowName, curr_frame,
+                                 tp, frame_index, file_name, result_dict, m_clicks_list, working_directory)
+        shouldReload = interface_return[0]
+        if shouldReload:
+            m_clicks_list = []
+    
     os.remove("temp.bmp")
+    return interface_return[1]
 
 
-def label_mutiple(start_pt, tp_list, vid_path):
-    file_name = str(os.path.basename(vid_path))
+def label_multiple(start_pt, tp_list, vid_path, result_dict, clicks_list, working_directory):
     total_frames_count = str(len(tp_list))
     tp_index = start_pt
+    m_result_dict = result_dict
+    m_clicks_list = clicks_list
     while tp_index < len(tp_list):
-        shouldReload = True
-        tp = tp_list[tp_index]
-        imgphon.get_video_frame(vid_path, tp)
-
-        while shouldReload:
-            curr_frame = cv2.imread("temp.bmp")
-            frame_index = str(tp_index + 1)
-            windowName = file_name + " (" + frame_index + "/" + total_frames_count + \
-                ") | [R] Reload | [N] Save & Next | [Q] Save & Quit"
-            cv2.namedWindow(windowName)
-            cv2.moveWindow(windowName, 320, 180)
-            cv2.setMouseCallback(windowName, paint_dot,
-                                 curr_frame)
-
-            shouldReload = interface(
-                windowName, curr_frame, tp, frame_index, file_name)
-
-            if not shouldReload:
-                tp_index += 1
-
-    os.remove("temp.bmp")
-
+        m_tp_index = tp_index + 1
+        new_result_dict = label_single(m_tp_index, vid_path, total_frames_count, m_result_dict, m_clicks_list, working_directory)
+        m_result_dict = new_result_dict
+        m_clicks_list = []
+        tp_index += 1
 
 def sort_coords(tmp_l):
-    tmp_dict = {}
-
     def take_x(elem):
         return elem[0]
     tmp_l.sort(key=take_x)
@@ -126,24 +105,27 @@ def sort_coords(tmp_l):
         upper = tmp_l[2]
         lower = tmp_l[1]
 
-    tmp_dict["leftx"] = left[0]
-    tmp_dict["lefty"] = left[1]
-    tmp_dict["rightx"] = right[0]
-    tmp_dict["righty"] = right[1]
-    tmp_dict["upperx"] = upper[0]
-    tmp_dict["uppery"] = upper[1]
-    tmp_dict["lowerx"] = lower[0]
-    tmp_dict["lowery"] = lower[1]
+    tmp_dict = {"leftx": left[0],
+                "lefty": left[1],
+                "rightx": right[0],
+                "righty": right[1],
+                "upperx": upper[0],
+                "uppery": upper[1],
+                "lowerx": lower[0],
+                "lowery": lower[1]}
     
     return tmp_dict
 
 if __name__ == "__main__":
+    result_dict = {}
+    clicks_list = []
+
     all_mode = "Start a new annotation"
     cont_mode = "Resume an unfinished work"
     single_mode = "Modify a single frame"
 
-    welcome_msg = """Please select an option to proceed. \n
-    NOTICE: You may only select the 2nd or the 3rd option on annotations 
+    welcome_msg = """Please select an option to proceed.\n
+    NOTICE: You may only modify a single frame for annotations
     that you already started or finished."""
 
     choice = easygui.buttonbox(
@@ -161,7 +143,7 @@ if __name__ == "__main__":
 
     # ALL MODE
     if choice == all_mode:
-        label_mutiple(0, m_tp_list, vid_path)
+        label_multiple(0, m_tp_list, vid_path, result_dict, clicks_list, working_directory)
     # RESUME MODE
     elif choice == cont_mode:
         with contextlib.suppress(FileNotFoundError):
@@ -175,7 +157,7 @@ if __name__ == "__main__":
                 start_tp = tp_index
                 break
             tp_index += 1
-        label_mutiple(start_tp, m_tp_list, vid_path)
+        label_multiple(start_tp, m_tp_list, vid_path, result_dict, clicks_list, working_directory)
     # SINGLE MODE
     elif choice == single_mode:
         try:
@@ -183,15 +165,15 @@ if __name__ == "__main__":
                 result_dict = np.load(os.path.join(
                     working_directory, "result_dict.npy")).item()
         except FileNotFoundError:
-            if easygui.msgbox(msg="""single mode is only for modifying existing project. 
-                                     You need to start one first""") == "OK":
+            if easygui.msgbox(msg="""Single mode is only for modifying existing project. \
+                                     You need to start one first.""") == "OK":
                 sys.exit(0)
         frame_index = easygui.integerbox(
             msg="Please enter the frame index you would like to modify", title='Frame Timepoint', 
             lowerbound=1, upperbound=(len(m_tp_list)))
         if frame_index == None:
             sys.exit(0)
-        label_single(frame_index, vid_path)
+        label_single(frame_index, vid_path, 0, result_dict, clicks_list, working_directory)
 
     # WRITING TO OUTPUT
     keys = ["leftx","lefty","rightx","righty","upperx","uppery","lowerx","lowery"]
